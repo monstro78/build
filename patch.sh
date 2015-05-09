15167498d82abf9bfb34a0b9afcddb45fcb9afa4
Giorgos Karag <giorgoskarag@gmail.com>
2015-05-09 22:34:52 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,5 +1,4 @@
-BASEDIR=$(pwd)
-
+echo -e $CL_BLU"Starting Patching..."$CL_RST
 echo -e $CL_BLU"Cherrypicking for android_system_core - Reboot/shutdown fix"$CL_RST
 cd system/core
 git fetch https://github.com/TeamCanjica/android_system_core cm-11.0
@@ -14,7 +13,7 @@ git cherry-pick de30387b3c32c2a9cf653590c8454bd002bf0dd1
 cd ..
 echo -e $CL_BLU"Cherrypicking for android_frameworks_native - Legacy sensors"$CL_RST
 cd native
-git fetch http://review.cyanogenmod.org/Cyanog...meworks_native refs/changes/11/59311/1
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/11/59311/1
 git cherry-pick FETCH_HEAD
 cd ../..
 echo -e $CL_BLU"Cherrypicking for android_external_chromium_org - Video streaming fix"$CL_RST
@@ -39,27 +38,27 @@ git cherry-pick 3dba426da410fadcd757eaa131166e534afbd1a9
 cd ..
 echo -e $CL_BLU"Cherrypicking for android_frameworks_native - STE Multimedia Fix [2/4]"$CL_RST
 cd native
-git fetch https://github.com/TeamCanjica/andro...meworks_native cm-11.0
+git fetch https://github.com/TeamCanjica/android_frameworks_native cm-11.0
 git cherry-pick f5a8698ce9a3568cea95c03302deb068eff765bd
 cd ../..
 echo -e $CL_BLU"Cherrypicking for android_system_vold - patch to allow switching storages"$CL_RST
 cd system/vold
-git fetch http://review.cyanogenmod.org/Cyanog...id_system_vold refs/changes/15/56515/2
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_vold refs/changes/15/56515/2
 git cherry-pick FETCH_HEAD
 cd ../..
 echo -e $CL_BLU"Cherrypicking for android_packages_services_Telephony - Low-InCall fix"$CL_RST
 cd packages/services/Telephony
-git fetch https://github.com/TeamCanjica/andro...ices_Telephony cm-11.0
+git fetch https://github.com/TeamCanjica/android_packages_services_Telephony cm-11.0
 git cherry-pick fdf281fdabe5e7517eb96f2faf159bbcc74ae4a6
 cd ../../..
-
 echo -e $CL_BLU"Cherrypicking vibrator fix"$CL_RST
 cd hardware/libhardware_legacy
-git fetch https://github.com/TeamCanjica/andro...ardware_legacy cm-11.0
+git fetch https://github.com/TeamCanjica/android_hardware_libhardware_legacy cm-11.0
 git cherry-pick 9c2250d32a1eda9afe3b5cefe3306104148aa532
 cd ../..
 echo -e $CL_BLU"Cherrypicking Camera fix"$CL_RST
 cd packages/apps/Camera2
-git fetch https://github.com/CyanogenMod/andro...s_apps_Camera2 cm-11.0
+git fetch https://github.com/CyanogenMod/android_packages_apps_Camera2 cm-11.0
 git cherry-pick 42067bbce2203088e09039169b0262691dd07e97
 cd ../../..
+echo -e $CL_BLU"DONE!"$CL_RST
