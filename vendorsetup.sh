5c7123e1ed6d84515a685c1548b8a7a31d14abe7
Giorgos Karag <giorgoskarag@gmail.com>
2015-05-10 14:10:28 +0300

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+add_lunch_combo pa_deb-userdebug
+add_lunch_combo pa_grouper-userdebug
+add_lunch_combo pa_flo-userdebug
+add_lunch_combo pa_hammerhead-userdebug
+add_lunch_combo pa_tilapia-userdebug
+add_lunch_combo pa_mako-userdebug
+add_lunch_combo pa_manta-userdebug
+add_lunch_combo pa_maguro-userdebug
+add_lunch_combo pa_toro-userdebug
+add_lunch_combo pa_toroplus-userdebug
+add_lunch_combo pa_codina-userdebug
